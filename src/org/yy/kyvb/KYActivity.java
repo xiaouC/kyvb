2bc6e81df1031e4d25ca619baf5404e19f9aa7ac
xiaou <xiaotjyu@gmail.com>
2016-02-28 16:18:13 +0800

    init baidu push

--- src/org/yy/kyvb/KYActivity.java
+++ src/org/yy/kyvb/KYActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.util.Log;
 
+import com.baidu.android.pushservice.CustomPushNotificationBuilder;
+import com.baidu.android.pushservice.PushConstants;
+import com.baidu.android.pushservice.PushManager;
+
 public class KYActivity extends Activity
 {
     /** Called when the activity is first created. */
@@ -17,6 +21,9 @@ public class KYActivity extends Activity
         Intent intentService = new Intent( KYActivity.this, VoiceBroadcastService.class );
         intentService.putExtra( "cacheDir", getCacheDir().getPath() );
         startService( intentService );
+
+        // baidu push
+        PushManager.startWork( this, PushConstants.LOGIN_TYPE_API_KEY, "api_key" );
     }
 
     @Override
