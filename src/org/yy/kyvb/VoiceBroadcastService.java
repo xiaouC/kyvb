ee04304d26d7f166fe04fd801f25cb73afc6871a
xiaou <xiaotjyu@gmail.com>
2016-02-29 20:33:29 +0800

    请求数据

--- src/org/yy/kyvb/VoiceBroadcastService.java
+++ src/org/yy/kyvb/VoiceBroadcastService.java
@@ -2,10 +2,16 @@ package org.yy.kyvb;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.io.File;
+import org.json.JSONObject;
+import org.json.JSONException;
 
+import java.util.Iterator;
+import java.io.PrintWriter;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,6 +20,9 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationCompat.Builder;
@@ -29,6 +38,9 @@ import android.util.Log;
 import android.content.BroadcastReceiver;
 import android.content.IntentFilter;
 import android.content.Intent;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class VoiceBroadcastService extends Service {
     private static final String TAG = "cocos";
@@ -50,7 +62,8 @@ public class VoiceBroadcastService extends Service {
 
     public static boolean bIsDestroy = false;
 
-    private static final String REQUEST_URL = "http://pokemonpro.sdk.dnastdio.com:8888/sdk/android/sdk/huawei/buoy";
+    private static final String VERIFY_URL = "http://pay.kuan1.cn/kysound/index/store";
+    private static final String BROADCAST_MESSAGE_URL = "http://pay.kuan1.cn/kysound/index/get";
 
     private interface ItemInfo {
         public String getItemType();
@@ -59,9 +72,17 @@ public class VoiceBroadcastService extends Service {
     private List<ItemInfo> recvItems = new ArrayList<ItemInfo>();
 
     public static String saveDir = "";
+    public static String ky_key = "ky_@0239732%^%$KEY";
+    public static String ky_comid = "ky123456_96";
+
     public boolean isFlySpeaking = false;
     public VoicePlayer mVoicePlayer = null;
     public VoiceTypeConfig mVoiceTypeConfig = null;
+    public int mScheduleIndex = -1;
+
+    public interface onResponseListener {
+        public void onResponse( String data );
+    }
 
     @Override
     public void onCreate() {
@@ -99,22 +120,28 @@ public class VoiceBroadcastService extends Service {
         mVoiceTypeConfig = new VoiceTypeConfig();
         mVoicePlayer = new VoicePlayer();
 
-        for( int i=1; i < 11; ++i ) {
-            final int index = i;
-            recvItems.add( new ItemInfo() {
-                public String getItemType() { return VoiceTypeConfig.VT_PAY_INFO; }
-                public List<String> getSpeakContent() {
-                    List<String> ret_speak_list = new ArrayList<String>();
-                    ret_speak_list.add( formatMoney( index + 100.5f ) );
-                    return ret_speak_list;
-                }
-            });
-        }
+        //for( int i=1; i < 11; ++i ) {
+        //    final int index = i;
+        //    recvItems.add( new ItemInfo() {
+        //        public String getItemType() { return VoiceTypeConfig.VT_PAY_INFO; }
+        //        public List<String> getSpeakContent() {
+        //            List<String> ret_speak_list = new ArrayList<String>();
+        //            ret_speak_list.add( formatMoney( index + 100.5f ) );
+        //            return ret_speak_list;
+        //        }
+        //    });
+        //}
+
+        //YYSchedule.getInstance().scheduleOnceTime( 1000, new YYSchedule.onScheduleAction() {
+        ////mScheduleIndex = YYSchedule.getInstance().scheduleCircle( 1000, new YYSchedule.onScheduleAction() {
+        //    public void doSomething() {
+        //        processData();
+        //    }
+        //});
 
         YYSchedule.getInstance().scheduleOnceTime( 1000, new YYSchedule.onScheduleAction() {
-        //mScheduleIndex = YYSchedule.getInstance().scheduleCircle( 1000, new YYSchedule.onScheduleAction() {
             public void doSomething() {
-                processData();
+                requestVerify();
             }
         });
     }
@@ -238,32 +265,206 @@ public class VoiceBroadcastService extends Service {
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    public void requestData() {
-        try {
-            URL url = new URL( REQUEST_URL );
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setDoOutput( false ); // 是否输入参数
-            connection.connect();
+    public String getTimestamp() {
+        Long tsLong = System.currentTimeMillis() / 1000;
+        return tsLong.toString();
+    }
 
-            BufferedReader reader = new BufferedReader( new InputStreamReader( connection.getInputStream() ) );
+    public String stringToMD5( String str ) {
+        try {
+            byte[] hash = MessageDigest.getInstance( "MD5" ).digest( str.getBytes( "UTF-8" ) );
+
+            StringBuilder hex = new StringBuilder( hash.length * 2 );
+            for( byte b : hash ) {
+                if( ( b & 0xFF ) < 0x10 )
+                    hex.append("0");
+                hex.append( Integer.toHexString( b & 0xFF ) );
+            }
+
+            return hex.toString();
+        } catch ( NoSuchAlgorithmException e ) {
+            throw new RuntimeException( "Huh, MD5 should be supported?", e );
+        } catch ( UnsupportedEncodingException e ) {
+            throw new RuntimeException( "Huh, UTF-8 should be supported?", e );
+        }
+
+        //return "";
+    }
+
+    public void requestVerify() {
+        String ts = getTimestamp();
+        Log.v( "cocos", "timestamp : " + ts );
+        Log.v( "cocos", "comid : " + ky_comid );
+        Log.v( "cocos", "ky_key : " + ky_key );
+        String md5 = stringToMD5( ky_comid + ky_key + ts );
+        Log.v( "cocos", "md5 : " + md5 );
+
+        Map<String,String> postParams = new HashMap<String,String>();
+        postParams.put( "comid", ky_comid );
+        postParams.put( "time", ts );
+        postParams.put( "ticket", md5 );
+
+        requestData( VERIFY_URL, postParams, new onResponseListener() {
+            public void onResponse( String data ) {
+                Log.v( "cocos", "response data : " + data );
+                try {
+                    JSONObject roleInfo = new JSONObject( data );
+
+                    int status = roleInfo.getInt( "status" );
+                    Log.v( "cocos", "status : " + status );
+                    if( status == 1 ) {     // 成功
+                        String msg = roleInfo.getString( "msg" );
+                        Log.v( "cocos", "msg : " + msg );
+                        int times = roleInfo.getInt( "times" );
+                        Log.v( "cocos", "times : " + times );
+                        String store_name = roleInfo.getString( "store_name" );
+                        Log.v( "cocos", "store_name : " + store_name );
+
+                        // 马上请求一次
+                        requestBroadcastMessage();
+
+                        // 定时请求
+                        YYSchedule.getInstance().cancelSchedule( mScheduleIndex );
+                        mScheduleIndex = YYSchedule.getInstance().scheduleCircle( times * 1000, new YYSchedule.onScheduleAction() {
+                            public void doSomething() {
+                                requestBroadcastMessage();
+                            }
+                        });
+                    } else {
+                        // 不成功，5 秒后重新尝试
+                        YYSchedule.getInstance().scheduleOnceTime( 5000, new YYSchedule.onScheduleAction() {
+                            public void doSomething() {
+                                requestVerify();
+                            }
+                        });
+                    }
+                } catch ( JSONException e ) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void requestBroadcastMessage() {
+        String ts = getTimestamp();
+        String md5 = stringToMD5( ky_comid + ky_key + ts );
+
+        Map<String,String> postParams = new HashMap<String,String>();
+        postParams.put( "comid", ky_comid );
+        postParams.put( "time", ts );
+        postParams.put( "ticket", md5 );
+
+        requestData( BROADCAST_MESSAGE_URL, postParams, new onResponseListener() {
+            public void onResponse( String data ) {
+                Log.v( "cocos", "response data : " + data );
+                try {
+                    JSONObject roleInfo = new JSONObject( data );
+
+                    int status = roleInfo.getInt( "status" );
+                    Log.v( "cocos", "status : " + status );
+                    if( status == 1 ) {     // 成功
+                        String msg = roleInfo.getString( "msg" );
+                        Log.v( "cocos", "msg : " + msg );
+                        String orderid = roleInfo.getString( "orderid" );
+                        Log.v( "cocos", "orderid : " + orderid );
+                        final double money = roleInfo.getDouble( "money" );
+                        Log.v( "cocos", "money : " + money );
+                        String order_time = roleInfo.getString( "order_time" );
+                        Log.v( "cocos", "order_time : " + order_time );
+
+                        recvItems.add( new ItemInfo() {
+                            public String getItemType() { return VoiceTypeConfig.VT_PAY_INFO; }
+                            public List<String> getSpeakContent() {
+                                List<String> ret_speak_list = new ArrayList<String>();
+                                ret_speak_list.add( formatMoney( money ) );
+                                return ret_speak_list;
+                            }
+                        });
+
+                        processData();
+                    }
+                } catch ( JSONException e ) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void requestData( String requestURL, Map<String,String> postParams, onResponseListener rspListener ) {
+        Log.v( "cocos", "requestURL : " + requestURL );
+
+        PrintWriter writer = null;  
+        BufferedReader reader = null;
+        HttpURLConnection connection = null;
+
+        StringBuffer params = new StringBuffer();
+        Iterator it = postParams.entrySet().iterator();
+        while( it.hasNext() ) {
+            Map.Entry element = (Map.Entry) it.next();
+            params.append(element.getKey());
+            params.append("=");
+            params.append(element.getValue());
+            params.append("&");
+        }
+        if( params.length() > 0 ) {
+            params.deleteCharAt( params.length() - 1 );
+        }
+
+        try {
+            URL url = new URL( requestURL );
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestProperty( "accept", "*/*" );
+            connection.setRequestProperty( "connection", "Keep-Alive" );
+            connection.setRequestProperty( "Content-Length", String.valueOf( params.length() ) );
+            connection.setDoOutput( true );
+            connection.setDoInput( true );
+            // 获取URLConnection对象对应的输出流 
+            writer = new PrintWriter( connection.getOutputStream() );
+            // 发送请求参数 
+            writer.write( params.toString() );
+            // flush输出流的缓冲 
+            writer.flush();
+
+            // 根据ResponseCode判断连接是否成功 
+            int responseCode = connection.getResponseCode();
+            if( responseCode != 200 ) {
+                Log.v( TAG, "requestVerify response code : " + responseCode );
+            }
+            //connection.connect();
+
+            reader = new BufferedReader( new InputStreamReader( connection.getInputStream() ) );
             StringBuffer readbuff = new StringBuffer();
             String lstr = null;
             while( ( lstr = reader.readLine() ) != null ) {
                 readbuff.append( lstr );
             }
-            connection.disconnect();
-            reader.close();
 
             String recvData = readbuff.toString();
             Log.v( TAG, "recvData : " + recvData );
+
+            rspListener.onResponse( recvData );
         } catch( MalformedURLException e ) {
             e.printStackTrace();
         } catch( IOException e ) {
             e.printStackTrace();
+        } finally {
+            if( connection != null ) {
+                connection.disconnect();
+            }
+            try {  
+                if( writer != null ) {
+                    writer.close();
+                }
+                if( reader != null ) {
+                    reader.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
-    public String formatMoney( float money ) {
+    public String formatMoney( double money ) {
         String ret_text = String.format( "%.02f", money );
         Log.v( "cocos", "formatMoney money : " + money );
         Log.v( "cocos", "formatMoney ret_text : " + ret_text );
@@ -289,24 +490,24 @@ public class VoiceBroadcastService extends Service {
             ItemInfo item = recvItems.get( 0 );
             recvItems.remove( 0 );
 
-            //FlyHelper.getInstance().startSpeaking( "已收到" + item.getSpeakContent().get( 0 ) + "元付款", "", new FlyHelper.onFlySpeakListener() {
-            //    public void onCompleted() {
-            //        delayNextProcessData();
-            //    }
-            //});
-            mVoiceTypeConfig.getSpeakFiles( item.getItemType(), item.getSpeakContent(), new VoiceTypeConfig.getFilesListener() {
-                public void onFilesReady( List<String> files ) {
-                    if( files != null ) {
-                        mVoicePlayer.play( files, new VoicePlayer.onPlayEndListener() {
-                            public void onPlayEnd() {
-                                delayNextProcessData();
-                            }
-                        });
-                    } else {
-                        delayNextProcessData();
-                    }
+            FlyHelper.getInstance().startSpeaking( "已收到付款：" + item.getSpeakContent().get( 0 ) + "元", "", new FlyHelper.onFlySpeakListener() {
+                public void onCompleted() {
+                    delayNextProcessData();
                 }
             });
+            //mVoiceTypeConfig.getSpeakFiles( item.getItemType(), item.getSpeakContent(), new VoiceTypeConfig.getFilesListener() {
+            //    public void onFilesReady( List<String> files ) {
+            //        if( files != null ) {
+            //            mVoicePlayer.play( files, new VoicePlayer.onPlayEndListener() {
+            //                public void onPlayEnd() {
+            //                    delayNextProcessData();
+            //                }
+            //            });
+            //        } else {
+            //            delayNextProcessData();
+            //        }
+            //    }
+            //});
         }
     }
 
